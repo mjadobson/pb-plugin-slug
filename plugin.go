11f9bd9204960cd4faeb3e1d13c7bd0f39d72eaa
mjadobson <mjadobson@gmail.com>
2026-04-01 21:56:14 +0200

    fix init

--- plugin.go
+++ plugin.go
@@ -58,10 +58,6 @@ func (p *Plugin) Init(app core.App) error {
 		return err
 	}
 
-	if err := p.validateStartupConfigs(app); err != nil {
-		return err
-	}
-
 	for _, cfg := range p.Configs {
 		config := cfg
 
